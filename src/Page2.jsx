175b1d65221277b7c9768dbec408be98ae536732
yazawayuma1203 <110238678+yazawayuma1203@users.noreply.github.com>
2022-08-11 23:35:25 +0900

    404ページ 404ページの作成

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -4,7 +4,7 @@ export const Page2 = () => {
   return (
     <div>
       <h1>Page2ページです</h1>
-      <Link to="/page2/999">URL Parameter</Link>
+      <Link to="/page2/999?name=hoge">URL Parameter</Link>
       <br />
       <Link to="/page2/999?name=hogehoge">Query Parameter</Link>
     </div>
