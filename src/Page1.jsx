5826bdd275ffa1d086c681b993eda8d9cc22179c
yazawayuma1203 <110238678+yazawayuma1203@users.noreply.github.com>
2022-08-11 23:16:55 +0900

    stateを渡す stateを渡す

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
   return (
     <div>
       <h1>Page1ページです</h1>
-      <Link to="/page1/detailA">detailA</Link>
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>detailA</Link>
       <br />
       <Link to="/page1/detailB">detailB</Link>
     </div>
